5b9690e87e0ad368bde1a8206643904e738fb975
Azzasafah <muhammad.hafizh2016@gmail.com>
2024-04-01 22:46:52 +0700

    upload chapter3 challenge

---
+++ chapter3.js
@@ -0,0 +1,44 @@
+let result
+let userInput 
+let check
+
+function oddNumber(bil){
+    result = bil % 2 != 0 
+    check = result ? "true" : "false";
+    return check
+}
+
+console.log("Hasilnya: " + oddNumber(2))
+
+
+function evenNumber(bil){
+    result = bil % 2 != 0 
+    check = result ? "true" : "false";
+    return check 
+}
+
+console.log("Hasilnya: " + evenNumber(2))
+
+function reverseString(str) { 
+    result =  str.split('').reverse().join('')
+    return result 
+} 
+
+console.log("Hasilnya: " + reverseString('age'))
+
+
+function upperString(str) { 
+    result =  str.toUpperCase();
+    return result 
+} 
+
+console.log("Hasilnya: " + upperString('age'))
+  
+
+function cirle(r) { 
+    result =  Math.PI * r * r
+    return result 
+}
+
+console.log("Hasilnya: " + cirle(6))
+
